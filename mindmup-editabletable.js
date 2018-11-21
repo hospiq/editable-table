eda7240c5860bff09f7832ed12a88eba311eb369
dmaftei <dan@hospiq.com>
2018-11-21 14:21:31 -0500

    try css() instead of outerWidth()

--- mindmup-editabletable.js
+++ mindmup-editabletable.js
@@ -20,8 +20,8 @@ $.fn.editableTableWidget = function (options) {
 						.show()
 						.offset(active.offset())
 						.css(active.css(activeOptions.cloneProperties))
-						.width(active.outerWidth())
-						.height(active.outerHeight())
+						.width(active.css('width'))
+						.height(active.css('height'))
 						.focus();
 					if (select) {
 						editor.select();
